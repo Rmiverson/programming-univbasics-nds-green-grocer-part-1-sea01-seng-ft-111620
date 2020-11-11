c0f1cfa73487ee750b805674c242293e1dc62b01
Riley Iverson <rmiverson99@gmail.com>
2020-11-11 23:56:17 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,8 +1,13 @@
+require 'pry'
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
+  collection.map{ |element|
+    element.map { |key, value|
+      if value == name
+        return element
+      end
+    }
+  }
+  return nil
 end
 
 def consolidate_cart(cart)
@@ -10,7 +15,9 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
+  retur
+  
+  
 end
 
 
