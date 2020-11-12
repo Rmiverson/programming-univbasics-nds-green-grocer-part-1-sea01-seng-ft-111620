49995e7261e58102f3f45cb50bac98565631dfd1
Riley Iverson <rmiverson99@gmail.com>
2020-11-12 04:22:10 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,7 +16,9 @@ def consolidate_cart(cart)
   
   cart.each { |element|
     element.each { |key, value|
-
+      if !ans[c]
+        ans[c] = { }
+      end
       ans[c][key] = value  
     }
     c += 1
