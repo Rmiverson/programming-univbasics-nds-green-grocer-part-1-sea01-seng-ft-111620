4cbc21ac76023b9d26999db813b5eb7d1d513247
Riley Iverson <rmiverson99@gmail.com>
2020-11-12 02:01:30 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -23,9 +23,9 @@ def consolidate_cart(cart)
     element.map { |key, value|
       if key != :count
         ans.push(element)
-        ans[c][:count] = 1
+        #ans[c][:count] = 1
       elsif ans[c][:item] == element[:item]
-        ans[c][:count] += 1
+        #ans[c][:count] += 1
       end
     }
     c += 1
