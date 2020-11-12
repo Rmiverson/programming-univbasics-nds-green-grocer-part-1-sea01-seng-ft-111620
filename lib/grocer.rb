d23fc447079e14fc5eb7ac33f8c916c66c4af1c4
Riley Iverson <rmiverson99@gmail.com>
2020-11-12 01:20:36 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -24,8 +24,8 @@ def consolidate_cart(cart)
     if !re_array[element]
       re_array.push(element)
       element.push(:count => 1)
-    elsif 
-      element[:count] += 1
+    else
+      
     end
   }
   puts re_array
